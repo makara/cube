b4920d962e01745a8a668ccab8e18a24dd963f12
Makara Wang <makara15@gmail.com>
2011-09-04 02:44:39 +0800

    change clear-block to clearfix; display header

--- page.tpl.php
+++ page.tpl.php
@@ -1,13 +1,16 @@
 <?php include('header.inc'); ?>
 
-<div id='page' class='clear-block limiter page-content'>
-  <?php if ($show_messages && $messages): ?>
-    <div id='console' class='clear-block'><?php print $messages; ?></div>
-  <?php endif; ?>
+<?php if (!empty($page['header'])): ?>
+  <div id='header' class='limiter clearfix'><?php print render($page['header']) ?></div>
+<?php endif; ?>
 
+<div id='page' class='clearfix limiter page-content'>
+  <?php if ($show_messages && $messages): ?>
+    <div id='console' class='clearfix'><?php print $messages; ?></div>
+  <?php endif; ?>
   <div id='content'>
     <?php if (!empty($page['content'])): ?>
-      <div class='content-wrapper clear-block'><?php print render($page['content']) ?></div>
+      <div class='content-wrapper clearfix'><?php print render($page['content']) ?></div>
     <?php endif; ?>
   </div>
 </div>
