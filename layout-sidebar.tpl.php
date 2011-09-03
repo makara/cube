05ab2bcd994e15314a357282a3cfb0fe980944f2
Makara Wang <makara15@gmail.com>
2011-09-04 02:54:40 +0800

    layouts

---
+++ layout-sidebar.tpl.php
@@ -0,0 +1,21 @@
+<?php include('header.inc'); ?>
+
+<?php if (!empty($page['header'])): ?>
+  <div id='header' class='limiter clearfix'><?php print render($page['header']) ?></div>
+<?php endif; ?>
+
+<div id='page' class='clearfix limiter page-content'>
+  <?php if ($show_messages && $messages): ?>
+    <div id='console' class='clearfix'><?php print $messages; ?></div>
+  <?php endif; ?>
+  <div id='content'>
+    <?php if (!empty($page['content'])): ?>
+      <div class='content-wrapper clearfix'><?php print render($page['content']) ?></div>
+    <?php endif; ?>
+  </div>
+  <div id='right' class='clearfix'>
+    <?php if (!empty($page['sidebar_second'])) print render($page['sidebar_second']) ?>
+  </div>
+</div>
+
+<?php include('footer.inc'); ?>
